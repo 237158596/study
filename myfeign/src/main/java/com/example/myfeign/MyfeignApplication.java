9343e74f8b98bb19f4fbfddad24731d8bb9a319a
曹宏江 <hongjiangcao@pateo.com.cn>
2021-02-23 09:37:03 +0800

    myfeignclient

---
+++ myfeign/src/main/java/com/example/myfeign/MyfeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfeignApplication.class, args);
+    }
+
+}
